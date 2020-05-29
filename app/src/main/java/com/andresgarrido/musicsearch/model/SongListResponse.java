bca6e56f01c8edcedb06d120126009cf813d5920
andres <buster092@gmail.com>
2020-05-28 21:27:55 -0400

    added response models

---
+++ app/src/main/java/com/andresgarrido/musicsearch/model/SongListResponse.java
@@ -0,0 +1,8 @@
+package com.andresgarrido.musicsearch.model;
+
+import java.util.List;
+
+public class SongListResponse {
+	public int resultCount;
+	public List<SongResponse> results;
+}
---
+++ app/src/main/java/com/andresgarrido/musicsearch/model/SongResponse.java
@@ -0,0 +1,24 @@
+package com.andresgarrido.musicsearch.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class SongResponse {
+
+	public String artworkUrl100;
+
+	public String artworkUrl30;
+
+	@SerializedName("collectionId")
+	public long albumId;
+
+	@SerializedName("collectionName")
+	public String albumName;
+
+	@SerializedName("artistName")
+	public String bandName;
+
+	public String trackName;
+
+	@SerializedName("primaryGenreName")
+	public String genre;
+}
